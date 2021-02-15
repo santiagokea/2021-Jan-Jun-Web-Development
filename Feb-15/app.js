5561c2b45d5b938c02b6700dab800fe85d15e788
Santiago Donoso <sand@kea.dk>
2021-02-15 13:15:49 +0100

    ...

--- Feb-15/app.js
+++ Feb-15/app.js
@@ -10,6 +10,7 @@ function create_item(){
     items.push(item_name)
     // save it to the localStorage, but this is an array... we need text
     localStorage.items = JSON.stringify(items)
+    render_item(item_name)
   }else{
     console.log("no, items not there. It is empty")
     const items = []
@@ -18,6 +19,7 @@ function create_item(){
     items_array_as_text = JSON.stringify(items)
     // save the items text in localStorage
     localStorage.items = items_array_as_text
+    render_item(item_name)
   }
 }
 
@@ -27,18 +29,21 @@ function show_items(){
   if( localStorage.items ){
     // convert the items to an array
     const array_items = JSON.parse(localStorage.items) // text to array
-    array_items.forEach( item => {
-      let div_item = `
-      <div class="item">
-        <div>1</div>
-        <div>${item}</div>
-        <button>delete item</button>
-      </div>`
-      document.querySelector("#items").insertAdjacentHTML('beforeend', div_item)
-    })
+    array_items.forEach( item => render_item(item) )
   }
 }
 show_items()
 
 
+
+// ##############################
+function render_item(item){
+  let div_item = `
+  <div class="item">
+    <div>1</div>
+    <div>${item}</div>
+    <button>delete item</button>
+  </div>`
+  document.querySelector("#items").insertAdjacentHTML('beforeend', div_item)
+}
 
