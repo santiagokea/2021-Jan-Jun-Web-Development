ec9729d5a11388616ef77cd2c3551dae4adea407
Santiago Donoso <sand@kea.dk>
2021-02-15 12:36:22 +0100

    ...

--- Feb-15/app.js
+++ Feb-15/app.js
@@ -1,5 +1,17 @@
 function create_item(){
   console.log("creating item...")
   let item_name = document.querySelector("#item_name").value
-  console.log(item_name)
+  
+  // check if there are items already in localStorage
+  if( localStorage.items ){
+    console.log("yes items already there")
+  }else{
+    console.log("no, items not there. It is empty")
+    const items = []
+    items.push(item_name) // ["a","b","c"]
+    // convert the items array into an items "array" text
+    items_array_as_text = JSON.stringify(items)
+    // save the items text in localStorage
+    localStorage.items = items_array_as_text
+  }
 }
