c2db95801b43e448737e4e972b4cbe705d16ec11
Santiago Donoso <sand@kea.dk>
2021-02-22 11:01:08 +0100

    ...

--- Feb-22/validator.js
+++ Feb-22/validator.js
@@ -1,3 +1,12 @@
 function validate(){
-  console.log("validating...")
+  // console.log(event.target)
+  const frm = event.target
+  all("[data-validate]").forEach( element => {
+    console.log(element)
+  })
 }
+
+
+
+function one(selector){ return document.querySelector(selector)}
+function all(selector){return document.querySelectorAll(selector)}
