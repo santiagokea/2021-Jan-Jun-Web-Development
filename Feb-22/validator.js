ddecc6720f6af5422cc3cd2898a4e8172a8407b6
Santiago Donoso <sand@kea.dk>
2021-02-22 11:11:02 +0100

    ...

--- Feb-22/validator.js
+++ Feb-22/validator.js
@@ -1,11 +1,20 @@
 function validate(){
   // console.log(event.target)
   const frm = event.target
+
+  // Clear errors
+  all("[data-validate]").forEach(element=>{
+    element.classList.remove("error")
+  })
+
   all("[data-validate]").forEach( element => {
     const min = element.getAttribute("data-min")
     const max = element.getAttribute("data-max")
     const total_characters = element.value.length
     console.log(total_characters)
+    if( total_characters < min ||  total_characters > max){
+      element.classList.add("error")      
+    }
   })
 }
 
