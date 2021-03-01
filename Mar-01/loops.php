3e3fe4202ba50bd6541edc4c632d7b9333bf5710
Santiago Donoso <sand@kea.dk>
2021-03-01 10:20:11 +0100

    ...

---
+++ Mar-01/loops.php
@@ -0,0 +1,26 @@
+<?php
+
+$user = ['name'=>'a', 'last_name'=>'b'];
+foreach($user as $key => $value){
+  echo "$key $value";
+}
+
+
+
+
+// arrays means many, many means loop
+// $letters = ['a', 'b', 'c'];
+// foreach($letters as $index=>$value){
+//   // echo $index; // 012
+//   // echo $value; // abc
+//   echo "$index $value";
+// }
+// foreach($letters as $letter){
+//   echo $letter;
+// }
+// sizeof or use count
+// for( $i = 0 ;  $i < count($letters); $i++ ){
+//   echo $letters[$i];
+// }
+
+
