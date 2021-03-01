ffe8fc38aa43711b64701ed3095fa92068ad82d0
Santiago Donoso <sand@kea.dk>
2021-03-01 11:01:26 +0100

    ...

---
+++ Mar-01/html_assoc.php
@@ -0,0 +1,19 @@
+<?php
+  $people = [
+    ['id'=>'a1', 'name'=>'a', 'last_name'=>'aa'],
+    ['id'=>'b2', 'name'=>'b', 'last_name'=>'bb'],
+    ['id'=>'c3', 'name'=>'c', 'last_name'=>'cc']
+  ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
