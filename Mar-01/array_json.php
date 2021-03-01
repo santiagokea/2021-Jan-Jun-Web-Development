c24f1fe2efc3b77f5161b1e21e897bde858ea188
Santiago Donoso <sand@kea.dk>
2021-03-01 12:22:54 +0100

    ...

---
+++ Mar-01/array_json.php
@@ -0,0 +1,19 @@
+<?php
+  $users = [];
+  $user_one = new stdClass();
+  $user_one->id = 1;
+  $user_one->name = 'a';
+  array_push($users, $user_one);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
