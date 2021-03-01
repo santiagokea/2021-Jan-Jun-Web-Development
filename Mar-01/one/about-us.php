ee768b5339f83cf2d7bb9ac6001ecb1f843704c7
Santiago Donoso <sand@kea.dk>
2021-03-01 12:39:06 +0100

    ...

---
+++ Mar-01/one/about-us.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My first website</title>
+</head>
+<body>
+
+  <nav>
+    <a href="index.php">index</a>
+    <a href="about-us.php">about us</a>
+  </nav>
+
+  <h1>About us</h1>
+</body>
+</html>
---
+++ Mar-01/one/index.php
@@ -0,0 +1,12 @@
+<?php
+  // require require_once include include_one
+  // require -> must be there or stop the page
+  // require_once -> use it only 1 time
+  // include -> nice it tries to use it
+  // include_one -> 1 time
+  require_once(__DIR__.'/top.php');
+?>
+
+  <h1>Welcome</h1>
+</body>
+</html>
