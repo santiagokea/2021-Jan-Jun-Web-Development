30630acbd01aa6c698b7b7ae9738f42c69b8855c
Santiago Donoso <sand@kea.dk>
2021-03-01 12:07:37 +0100

    ...

--- Mar-01/html_assoc.php
+++ Mar-01/html_assoc.php
@@ -11,11 +11,28 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="app.css">
   <title>Document</title>
 </head>
 <body>
   <div id="users">
-    
+    <?php
+    foreach($people as $person){
+    ?>
+      <div class="person">
+        <div>
+          ID: <?= $person['id'] ?>
+        </div>
+        <div>
+          Name: <?= $person['name'] ?>
+        </div>
+        <div>
+          Last name: <?= $person['last_name'] ?>
+        </div>
+      </div>    
+    <?php
+    }
+    ?>
   </div>
 </body>
 </html>
