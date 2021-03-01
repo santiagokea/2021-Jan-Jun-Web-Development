5ea4a18844fce5ac87ae6d25585cea610c075b8c
Santiago Donoso <sand@kea.dk>
2021-03-01 09:03:37 +0100

    ...

---
+++ Mar-01/test.php
@@ -0,0 +1,17 @@
+<?php
+// double quotes is not the same as single quotes
+// echo 'Santiago';
+// variable
+// $name = 'Santiago';
+// Concatenation you use a dot
+// The plus sign is used for math
+// echo 'Hi '.$name;
+// Double quoutes take longer time for PHP
+// echo "Hi $name";
+// If you don't use variable in the sentence
+// then use single quotes
+$name = 'A';
+$last_name = 'B';
+echo 'Hi '.$name.' '.$last_name;
+
+
