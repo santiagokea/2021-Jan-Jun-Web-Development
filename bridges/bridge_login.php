33b051f219fb7973b05a3145a7ec50b720bdee9d
Santiago Donoso <sand@kea.dk>
2021-03-15 10:39:49 +0100

    ...

--- bridges/bridge_login.php
+++ bridges/bridge_login.php
@@ -4,6 +4,8 @@
 require_once(__DIR__.'/../db.php');
 echo $_POST['user_email'];
 echo $_POST['user_password'];
-
+$q = $db->prepare('SELECT * FROM users WHERE email = :email ');
+$q
+// 10:40
 // header('Location: /login');
 
