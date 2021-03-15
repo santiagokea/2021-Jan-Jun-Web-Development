92d6ffbb682e904e52a477c1d4ae45f095992596
Santiago Donoso <sand@kea.dk>
2021-03-15 10:29:50 +0100

    ...

--- bridges/bridge_login.php
+++ bridges/bridge_login.php
@@ -1,6 +1,9 @@
 <?php
 // TODO: validate user_email
 // TODO: validate user_password
+require_once(__DIR__.'/../db.php');
+echo $_POST['user_email'];
+echo $_POST['user_password'];
 
-header('Location: /login');
+// header('Location: /login');
 
