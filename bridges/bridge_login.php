ed95870f60e61b619d53ac7bfde7ef2324440dd5
Santiago Donoso <sand@kea.dk>
2021-03-15 10:55:55 +0100

    ...

--- bridges/bridge_login.php
+++ bridges/bridge_login.php
@@ -6,11 +6,12 @@ require_once(__DIR__.'/../db.php');
 try{
   // echo $_POST['user_email'];
   // echo $_POST['user_password'];
-  $q = $db->prepare('SELECT * FROM users WHERE email = :email ');
+  // $q = $db->prepare('SELECT * FROM users WHERE email = :email ');
+  $q = $db->prepare("SELECT * FROM users WHERE email = '{$_POST['user_email']}'");
   // $q = $q->bindValue(':email', $_POST['user_email']);
-  // $q->execute();
-  // $user = $q->fetchAll();
-  // echo json_encode($user);
+  $q->execute();
+  $user = $q->fetchAll();
+  echo json_encode($user);
   // 10:40
   // header('Location: /login');
 }catch(PDOException $ex){
