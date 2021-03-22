5aafc482838b8f3427ba844adabb9c229169ea8f
Santiago Donoso <sand@kea.dk>
2021-03-22 10:13:51 +0100

    ...

--- apis/api_posts_like_or_dislike.php
+++ apis/api_posts_like_or_dislike.php
@@ -15,7 +15,8 @@ if( $like_or_dislike == 0){
   // UPDATE posts SET likes = likes +1 WHERE post_id = 1
   $number = file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/db/dislikes.txt"); // read the content of the file
   $number = $number+1;
-  echo $number;
+  // write back to the file. Second argument is the data to be written
+  file_put_contents("{$_SERVER['DOCUMENT_ROOT']}/db/dislikes.txt", $number);
   exit();
 }
 
