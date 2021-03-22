00f0e7e4a538be50d483f08d2f98448782e84736
Santiago Donoso <sand@kea.dk>
2021-03-22 12:59:20 +0100

    ...

--- views/view_posts.php
+++ views/view_posts.php
@@ -11,7 +11,19 @@
   
   <div id="posts">
   
-    <div class="post">
+    <div id="Post_1"  class="post" data-post-id="1">
+      <div>This is post one</div>
+      <button class="like" onclick="like(); return false">like</button>
+      <button class="dislike" onclick="dislike(); return false">dislike</button>    
+    </div>
+
+    <div id="Post_2"  class="post" data-post-id="2">
+      <div>This is post one</div>
+      <button class="like" onclick="like(); return false">like</button>
+      <button class="dislike" onclick="dislike(); return false">dislike</button>    
+    </div>
+
+    <div id="Post_3"  class="post" data-post-id="3">
       <div>This is post one</div>
       <button class="like" onclick="like(); return false">like</button>
       <button class="dislike" onclick="dislike(); return false">dislike</button>    
@@ -25,8 +37,10 @@
     async function like(){
       let button = event.target     
       let button_parent = button.parentNode
-      console.log(button_parent) 
-      let conn = await fetch('/posts/1/1', {
+      // let post_id = button_parent.id
+      let post_id_from_data_attr = button_parent.getAttribute("data-post-id")
+      console.log(post_id_from_data_attr) 
+      let conn = await fetch(`/posts/${post_id_from_data_attr}/1`, {
         method:"POST"
       })
       // if( conn.status != 200 ){ alert("something went wrong") }
