51ca321a3d507ab8e2ea562fc97430cb08ded298
Santiago Donoso <sand@kea.dk>
2021-03-22 13:27:13 +0100

    ...

---
+++ Mar-15/views/view_top.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $page_title ?? 'COMPANY' ?></title>
+</head>
+<body>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="/">
+          home
+        </a>
+      </li>
+      <li>
+        <a href="/users">
+          users
+        </a>
+      </li>        
+      <li>
+        <a href="/login">
+          login
+        </a>
+      </li>      
+    </ul>
+  </nav>
