5edf494eb708deb38a21a413d971cad5b0972927
Santiago Donoso <sand@kea.dk>
2021-04-12 12:31:22 +0200

    ...

--- views/view_login.php
+++ views/view_login.php
@@ -3,8 +3,10 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/views/view_top.php');
 ?>
 
 
-<form action="/login" method="POST">
-  <input type="text" placeholder="email">
+<form action="/login" method="POST" onsubmit="return validate()">
+  <input type="text" placeholder="email"
+    data-validate="email"
+  >
   <input type="password" placeholder="password">
   <button>
     login
