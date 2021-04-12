8dde5e00aba25e46005a0222e306acbcab1536e0
Santiago Donoso <sand@kea.dk>
2021-04-12 09:46:14 +0200

    ...

--- routes.php
+++ routes.php
@@ -8,12 +8,24 @@ get('/', function(){
 });
 
 // ##############################
-post('/users', function(){
+post('/users/create', function(){
   echo 'user created';
 });
+
 // ##############################
 post('/users/update/:id', function($id){
   echo "Updating user with id: $id";
+});
+
+// ##############################
+post('/users/delete/:id', function($id){
+  echo "Updating user with id: $id";
+});
+
+
+// ##############################
+post('/db-create-users', function(){
+  require_once($_SERVER['DOCUMENT_ROOT'].'/db/db_create_users.php');
 });
 
 
