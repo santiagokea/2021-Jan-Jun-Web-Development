ef0036a1b1dc6dac96e539142494a7436afd2820
Santiago Donoso <sand@kea.dk>
2021-04-19 10:05:01 +0200

    ...

--- bridges/bridge_signup.php
+++ bridges/bridge_signup.php
@@ -1,1 +1,14 @@
-hi
+<?php
+
+// TODO: Validate all input fields
+
+// TODO: Connect to the database and insert the user
+
+// TODO: The password is hashed password_hash()
+
+// TODO: If the user was inserted correctly, the user will be taken to the login page
+
+// TODO: If the user could NOT be inserted, the user will be taken to the signup error page
+
+
+
