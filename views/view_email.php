96c3e7f979834c35e19dda77e5fcd9f6147ff1de
Santiago Donoso <sand@kea.dk>
2021-04-19 13:30:20 +0200

    ...

--- views/view_email.php
+++ views/view_email.php
@@ -17,7 +17,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 1;                      //Enable verbose debug output
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
