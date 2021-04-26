d5ff99ef9abd70e7c82f2ec923a930139772f882
Santiago Donoso <sand@kea.dk>
2021-04-26 09:40:24 +0200

    ...

--- apis/api-search.php
+++ apis/api-search.php
@@ -5,5 +5,13 @@ if( ! isset($_POST['search_for']) ){
   http_response_code(400);
   exit();
 }
+if( strlen($_POST['search_for']) < 2 ){
+  http_response_code(400);
+  exit();
+}
+if( strlen($_POST['search_for']) > 20 ){
+  http_response_code(400);
+  exit();
+}
 
 
