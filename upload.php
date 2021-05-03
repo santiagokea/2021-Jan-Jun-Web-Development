3d429cc52b71855505a83900abbed3882a899552
Santiago Donoso <sand@kea.dk>
2021-05-03 10:29:39 +0200

    ...

--- upload.php
+++ upload.php
@@ -10,18 +10,17 @@
 $valid_extensions = ['png', 'jpg', 'jpeg', 'gif', 'zip', 'pdf'];
 
 $image_type = mime_content_type($_FILES['my_picture']['tmp_name']); // image/png
-$extension = strrchr( $image_type , '/');
-$extension = ltrim($extension, '/'); // png, jpg...
+$extension = strrchr( $image_type , '/'); // /png ... /tmp ... /jpg
+$extension = ltrim($extension, '/'); // png ... jpg ... plain
 
 if( ! in_array($extension, $valid_extensions) ){
   echo "mmm.. hacking me?";
   exit();
 }
 
-
 $random_image_name = bin2hex(random_bytes(16)).".$extension";
 move_uploaded_file($_FILES['my_picture']['tmp_name'], "images/$random_image_name");
-
+echo 'File uploaded';
 
 
 
